4f93a8c5a92843f63cb36bedf48df5360af6b5c0
shady-cj <petersp2000@gmail.com>
2024-03-01 19:35:14 +0100

    changed header from fixed to absolute on mobile

--- src/sections/Header/index.jsx
+++ src/sections/Header/index.jsx
@@ -3,7 +3,7 @@ import menu from "../../assets/menu.svg"
 import translations from "../../translation/headerTranslation"
 const Index = ({ setOpenQuote, lang }) => {
     return (
-        <div className="fixed z-10 max-w-[inherit] top-[2.5rem] w-[inherit] px-4 lg:px-[5rem] xl:px-[7.5rem]" >
+        <div className="absolute md:fixed md:z-10 max-w-[inherit] top-[2.5rem] w-[inherit] px-4 lg:px-[5rem] xl:px-[7.5rem]" >
             <header className='relative w-[80vw] bg-[#0E0E1A] w-full rounded-[2rem] py-5 md:py-2 pr-3 pl-6 flex justify-between items-center'>
                 <div className="text-sm font-semibold leading-4 text-[#F3F3FB]">
                     mTOITURE
