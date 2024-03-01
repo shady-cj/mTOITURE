aecc13535ecca5b5ca1be4cb8fae3323082174ff
shady-cj <petersp2000@gmail.com>
2024-03-01 16:36:17 +0100

    mobile responsiveness

--- src/sections/Header/index.jsx
+++ src/sections/Header/index.jsx
@@ -4,7 +4,7 @@ import translations from "../../translation/headerTranslation"
 const Index = ({ setOpenQuote, lang }) => {
     return (
         <div className="fixed z-10 max-w-[inherit] top-[2.5rem] w-[inherit] px-4 lg:px-[5rem] xl:px-[7.5rem]" >
-            <header className='w-[80vw] bg-[#0E0E1A] w-full rounded-[2rem] py-5 md:py-2 pr-3 pl-6 flex justify-between items-center'>
+            <header className='relative w-[80vw] bg-[#0E0E1A] w-full rounded-[2rem] py-5 md:py-2 pr-3 pl-6 flex justify-between items-center'>
                 <div className="text-sm font-semibold leading-4 text-[#F3F3FB]">
                     mTOITURE
                 </div>
@@ -12,6 +12,14 @@ const Index = ({ setOpenQuote, lang }) => {
                     <img src={menu} alt="" />
                     Menu
                 </div>
+                {/* <nav className="flex flex-col items-center z-10 md:hidden absolute left-0 right-0 top-full">
+                    <a href="#about" className="py-6">{translations[lang][0]}</a>
+                    <a href="#gallery" className="py-6">{translations[lang][1]}</a>
+                    <a href="#services" className="py-6">{translations[lang][2]}</a>
+                    <a href="#reviews" className="py-6">{translations[lang][3]}</a>
+                    <a href="#contact" className="py-6">{translations[lang][4]}</a>
+                    <div className="border border-solid"></div>
+                </nav> */}
                 <nav className="hidden md:flex gap-6">
                     <a href="#about" className="px-3 rounded-lg text-[#C4C4CC] font-normal leading-[18.15px] text-[15px]">{translations[lang][0]}</a>
                     <a href="#gallery" className="px-3 rounded-lg text-[#C4C4CC] font-normal leading-[18.15px] text-[15px]">{translations[lang][1]}</a>
